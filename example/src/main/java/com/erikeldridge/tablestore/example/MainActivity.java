fb0623967d14ef5dc430bf0f6a031cd10d58bc71
Erik Eldridge <erik@twitter.com>
2016-07-20 00:16:19 -0700

    Migrate to materialized path

--- example/src/main/java/com/erikeldridge/tablestore/example/MainActivity.java
+++ example/src/main/java/com/erikeldridge/tablestore/example/MainActivity.java
@@ -22,12 +22,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 TableStore store = TableStore.open(activity);
-                String userId = "1";
-                store.put("users", userId, "name", "Ms. Foo", System.currentTimeMillis()/1000);
-                store.put("users", userId, "phone", "+1234567890", null);
-                store.put("users", userId, "email", "1@example.com", null);
-                final String phoneData = store.get("users", userId, "phone");
-                final Map<String, String> personData = store.get("users", userId);
+                store.put("users/1/name", "Ms. Foo", System.currentTimeMillis()/1000);
+                store.put("users/1/phone", "+1234567890");
+                store.put("users/1/email", "1@example.com");
+                final Map<String, String> phoneData = store.get("users/1/phone");
+                final Map<String, String> personData = store.get("users/1");
                 store.close();
                 activity.runOnUiThread(new Runnable() {
                     @Override
--- tablestore/src/main/java/com/erikeldridge/tablestore/TableStore.java
+++ tablestore/src/main/java/com/erikeldridge/tablestore/TableStore.java
@@ -10,60 +10,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TableStore {
-    final SQLiteOpenHelper helper;
+    final Context context;
+    final SQLiteDatabase db;
     public final static String TABLE = "tablestore_table";
-    public final static String COLUMN_TYPE = "type";
-    public final static String COLUMN_ID = "id";
-    public final static String COLUMN_ATTR = "attr";
+    public final static String COLUMN_PATH = "path";
     public final static String COLUMN_VALUE = "value";
     public final static String COLUMN_UPDATED = "updated";
     public final static String COLUMN_EXPIRES = "expires";
-    public TableStore(SQLiteOpenHelper helper) {
-        this.helper = helper;
+    public TableStore(Context context, SQLiteDatabase db) {
+        this.context = context;
+        this.db = db;
     }
     public static TableStore open(Context context){
-        return new TableStore(new Helper(context));
+        return new TableStore(context, new Helper(context).getWritableDatabase());
     }
     public void close(){
-        helper.close();
+        db.close();
     }
-    public void put(String type, String id, String attr, String value, Long expires) {
-
-        final ContentValues values = new ContentValues();
-        values.put(COLUMN_TYPE, type);
-        values.put(COLUMN_ID, id);
-        values.put(COLUMN_ATTR, attr);
-        values.put(COLUMN_VALUE, value);
-        values.put(COLUMN_EXPIRES, expires);
-        helper.getWritableDatabase().insertWithOnConflict(TABLE, "null", values,
+    public void put(String path, String value) {
+        put(path, value, null);
+    }
+    public Map<String, String> get(String path) {
+        return get(path, COLUMN_PATH+" desc", null);
+    }
+    public void put(String path, String value, Long expires) {
+        final ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_PATH, path);
+        contentValues.put(COLUMN_VALUE, value);
+        contentValues.put(COLUMN_EXPIRES, expires);
+        db.insertWithOnConflict(TABLE, "null", contentValues,
                 SQLiteDatabase.CONFLICT_REPLACE);
     }
-    public Map<String, String> get(String type, String id) {
-        final Cursor cursor = helper.getReadableDatabase().query(
-                TABLE, new String[]{COLUMN_ATTR, COLUMN_VALUE},
-                "type=? and id=? and (expires is null or expires='' or expires > updated)",
-                new String[]{type, id}, null, null, null, null);
+    public Map<String, String> get(String path, String orderBy, String limit) {
+        final Cursor cursor = db.query(
+                TABLE, new String[] {COLUMN_PATH, COLUMN_VALUE},
+                context.getString(R.string.sql_query, COLUMN_PATH, COLUMN_EXPIRES, COLUMN_UPDATED),
+                new String[]{path}, null, null, orderBy, limit);
         final Map<String, String> values = toMap(cursor);
         cursor.close();
         return values;
     }
-    public String get(String type, String id, String attr) {
-        final Cursor cursor = helper.getReadableDatabase().query(
-                TABLE, new String[]{COLUMN_ATTR, COLUMN_VALUE},
-                "type=? and id=? and attr=? and (expires is null or expires='' or expires > updated)",
-                new String[]{type, id, attr}, null, null, null, null);
-        final Map<String, String> values = toMap(cursor);
-        cursor.close();
-        return values.get(attr);
-    }
     public Cursor query(String sql, String[] args){
-        return helper.getReadableDatabase().rawQuery(sql, args);
+        return db.rawQuery(sql, args);
     }
     protected Map<String, String> toMap(Cursor cursor) {
         final Map<String, String> values = new HashMap<>();
         cursor.moveToFirst();
         while (cursor.getCount() > 0 && !cursor.isAfterLast()) {
-            values.put(cursor.getString(cursor.getColumnIndex(COLUMN_ATTR)),
+            values.put(cursor.getString(cursor.getColumnIndex(COLUMN_PATH)),
                     cursor.getString(cursor.getColumnIndex(COLUMN_VALUE)));
             cursor.moveToNext();
         }
@@ -72,15 +66,14 @@ public class TableStore {
     static class Helper extends SQLiteOpenHelper {
         final Context context;
         public Helper(Context context) {
-            super(context, "tablestore_v1.db", null, 1);
+            super(context, "tablestore.db", null, 1);
             this.context = context;
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL(context.getString(R.string.db_v1_create_table_query,
-                    TABLE, COLUMN_TYPE, COLUMN_ID, COLUMN_ATTR, COLUMN_VALUE,
-                    COLUMN_EXPIRES, COLUMN_UPDATED));
+            db.execSQL(context.getString(R.string.sql_create_table,
+                    TABLE, COLUMN_PATH, COLUMN_VALUE, COLUMN_EXPIRES, COLUMN_UPDATED));
         }
 
         @Override
