466c76d86a3418fc808a8fcdead42d770e0a6478
Erik Eldridge <erik@twitter.com>
2016-07-22 10:58:35 -0700

    Clarify easy and advanced usage

--- example/src/main/java/com/erikeldridge/treestore/example/MainActivity.java
+++ example/src/main/java/com/erikeldridge/treestore/example/MainActivity.java
@@ -24,20 +24,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 TreeStore store = TreeStore.open(activity);
+
+                // simple key-value
                 store.put("users/1/name", "Ms. Foo");
+                final Map<String, String> nameData = store.get("users/1/name"); // {"users/1/name":"Ms. Foo"}
+                store.delete("users/1/name");
+
+                // range query
                 store.put("users/1/phone", "+1234567890");
                 store.put("users/1/email", "1@example.com");
+                final Map<String, String> userData = store.get("users/1"); // {"users/1/name":"Ms. Foo", "users/1/email":"1@example.com"...}
+
+                // advanced key-value
                 store.put("users/1/location", "Chicago", new TTL(1, TimeUnit.HOURS));
-                final Map<String, String> phoneData = store.get("users/1/phone"); // {"users/1/phone":"+1234567890"}
-                store.delete("users/1/phone");
-                final Map<String, String> userData = store.get("users/1"); // {"users/1/name":"Ms. Foo", "users/1/email": "1@example.com"...}
-                final Map<String, String> usersData = store.get("users", "asc", 10); // first 10 users
                 store.clean(); // remove expired entries
+
+                // advanced range query
+                final Map<String, String> usersData = store.get("users", "asc", 10); // first 10 user entries
+
                 store.close();
                 activity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        view.setText(activity.getString(R.string.output, phoneData,
+                        view.setText(activity.getString(R.string.output, nameData,
                                 userData.toString(), usersData.toString()));
                     }
                 });
